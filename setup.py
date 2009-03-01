59d0060340d456847b98193d7187a8a4a5d87a96
Tom Lazar <tom@tomster.org>
2009-03-01 15:01:56 +0100

    just a little red tape...

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+setup(name='twitsilver',
+      version=version,
+      description="A Quicksilver client for twitter",
+      long_description="""\
+""",
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: MacOS X",
+        "Operating System :: MacOS :: MacOS X",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: Communications",
+      ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='Quicksilver twitter OSX',
+      author='Tom Lazar',
+      author_email='tom@tomster.org',
+      url='http://tomster.org/',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+          "py_Growl",
+          "twitter",
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )

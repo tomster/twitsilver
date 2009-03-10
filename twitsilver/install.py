502e91c7d89a0c227728966e2eb7b5e2374c07fa
Tom Lazar <tom@tomster.org>
2009-03-10 11:06:07 +0100

    register with growl upon installation (instead of each time a message is sent)

--- twitsilver/install.py
+++ twitsilver/install.py
@@ -2,16 +2,18 @@
 from os import path, mkdir, chmod
 import sys
 
+from growl import twitsilver_growl
+
 def install_action():
     """ Installs the Quicksilver action """
     DEST_DIR = path.join(path.expanduser("~"), 
         "Library/Application Support/Quicksilver/Actions")
 
     if not path.exists(DEST_DIR):
-        print "creating target directory!" ; 
+        print "Creating target directory!" ; 
         mkdir(DEST_DIR)
 
-    print "installing Quicksilver action"
+    print "Installing Quicksilver action"
     installer = """#!/usr/bin/env python
 # encoding: utf-8
 __requires__ = 'twitsilver'
@@ -26,10 +28,18 @@ sys.exit(
     installer_script.write(installer)
     installer_script.close()
     chmod(DEST_DIR + "/tweet.py", 0775)
-    print "Done! Now restart Quicksilver (command-control-q)"
+
+def register_growl():
+    """Register with the growl service"""
+    print "Registering with growl."
+    twitsilver_growl().register()
 
 def main(argv=None):
     install_action()
+    register_growl()
+    print "Done! Now restart Quicksilver (command-control-q)"
+    
+    
 
 if __name__ == "__main__":
     sys.exit(main())
